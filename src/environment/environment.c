516fe37c4f008018798bf876f8c07c53b82b082f
Ahmet Kerem Sezgin <asezgin@k1m05s02.42kocaeli.com.tr>
2025-07-28 11:28:44 +0300

    Enviromnet nromi fix

--- src/environment/environment.c
+++ src/environment/environment.c
@@ -1,53 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   environment.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asezgin <asezgin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/28 11:26:24 by asezgin           #+#    #+#             */
+/*   Updated: 2025/07/28 11:26:24 by asezgin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
-void create_env(char **env_line, t_all *all)
+void	create_env(char **env_line, t_all *all)
 {
-    t_env *new_env;
-    t_env *current;
+	t_env	*new_env;
+	t_env	*current;
 
-    new_env = (t_env *)safe_malloc(all->collector, sizeof(t_env));
-    new_env->key = strdup(env_line[0]);    // Duplicate key string
-    if (env_line[1])
-        new_env->value = strdup(env_line[1]);  // Duplicate value string (if exists)
-    else
-        new_env->value = NULL;
-    new_env->next = NULL;
-
-    if (all->env == NULL)
-        all->env = new_env;
-    else
-    {
-        current = all->env;
-        while (current->next != NULL)
-            current = current->next;
-        current->next = new_env;
-    }
+	new_env = (t_env *)safe_malloc(all->collector, sizeof(t_env));
+	new_env->key = strdup(env_line[0]);
+	if (env_line[1])
+		new_env->value = strdup(env_line[1]);
+	else
+		new_env->value = NULL;
+	new_env->next = NULL;
+	if (all->env == NULL)
+		all->env = new_env;
+	else
+	{
+		current = all->env;
+		while (current->next != NULL)
+			current = current->next;
+		current->next = new_env;
+	}
 }
 
-
-void put_env(t_all *all, char **env_list)
+void	put_env(t_all *all, char **env_list)
 {
-    int i = 0;
-    char *key;
-    char *value;
-    char *eq_pos;
+	int		i;
+	char	*key;
+	char	*value;
+	char	*eq_pos;
 
-    while (env_list[i])
-    {
-        eq_pos = ft_strchr(env_list[i], '=');
-        if (eq_pos)
-        {
-            key = ft_substr(env_list[i], 0, eq_pos - env_list[i]);
-            value = ft_strdup(eq_pos + 1);
-        }
-        else
-        {
-            key = ft_strdup(env_list[i]);
-            value = NULL; // No value, maybe?
-        }
-        create_env((char *[3]){key, value, NULL}, all);
-        free(key);
-        free(value);
-        i++;
-    }
+	i = 0;
+	while (env_list[i])
+	{
+		eq_pos = ft_strchr(env_list[i], '=');
+		if (eq_pos)
+		{
+			key = ft_substr(env_list[i], 0, eq_pos - env_list[i]);
+			value = ft_strdup(eq_pos + 1);
+		}
+		else
+		{
+			key = ft_strdup(env_list[i]);
+			value = NULL;
+		}
+		create_env((char *[3]){key, value, NULL}, all);
+		free(key);
+		free(value);
+		i++;
+	}
 }
