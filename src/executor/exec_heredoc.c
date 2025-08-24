7cba68056a0d728416d2e49774f0f816c936f09d
Mahmut Esat Yilmaz <mayilmaz@k1m21s03.42kocaeli.com.tr>
2025-08-24 20:02:19 +0300

    gpt denemesi

--- src/executor/exec_heredoc.c
+++ src/executor/exec_heredoc.c
@@ -3,105 +3,143 @@
 /*                                                        :::      ::::::::   */
 /*   exec_heredoc.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: asezgin <asezgin@student.42.fr>            +#+  +:+       +#+        */
+/*   By: mayilmaz <mayilmaz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/19 09:43:50 by asezgin           #+#    #+#             */
-/*   Updated: 2025/08/24 19:25:46 by asezgin          ###   ########.fr       */
+/*   Updated: 2025/08/24 19:59:00 by mayilmaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../include/minishell.h"
-#include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include "../../include/minishell.h"
 
-static int	read_heredoc_input(int write_fd, char *eof, t_all *all)
+static int  read_heredoc_input(int write_fd, char *eof, t_all *all, int is_last)
 {
-	char	*line;
+    char    *line;
 
-	line = readline("> ");
-	while (line)
-	{
-		if (strcmp(line, eof) == 0)
-		{
-			free(line);
-			break ;
-		}
-		if (check_here_flag(all->card, eof))
-			line = here_expand(line, all);
-		write(write_fd, line, ft_strlen(line));
-		write(write_fd, "\n", 1);
-		free(line);
-		line = readline("> ");
-	}
-	return (0);
+	is_last = 0;
+    while (1)
+    {
+        /* Her satır için heredoc promptunu yazdır */
+        write(STDOUT_FILENO, "> ", 2);
+        /* Boş prompt ile readline çağırıyoruz; böylece kendi yazdığımız "> " promptu bozulmaz */
+        line = readline("");
+        if (!line)
+            return (1); /* ctrl-D veya EOF durumunda hata döndürülür */
+
+        /* Delimiter ile karşılaşırsak döngüden çık */
+        if (strcmp(line, eof) == 0)
+        {
+            free(line); /* Sadece son here-doc bitiminde yeni satır */
+            break;
+        }
+        /* Değişken genişletmesi gerekiyorsa */
+        if (check_here_flag(all->card, eof))
+            line = here_expand(line, all);
+
+        /* Eğer yazılacak fd geçerli ise, içeriği pipe’a yazdır */
+        if (write_fd >= 0)
+        {
+            write(write_fd, line, ft_strlen(line));
+            write(write_fd, "\n", 1);
+        }
+        free(line);
+    }
+    return (0);
 }
 
-char **last_heredoc(t_all *all)
+/* Tüm heredoc’ları işler; yalnızca sonuncusunun çıktısı komutun stdin’ine bağlanır */
+static void heredoc_loop(t_all *all, char **heredocs, int pipefd[2])
 {
-	char **last;
-	int heredoc_count;
+    int i;
 
-	heredoc_count = 0;
-	while (all->cmd->redirects)
-	{
-		if (all->cmd->redirects->type == HEREDOC)
-		{
-			last[heredoc_count] = all->cmd->redirects->filename;
-			heredoc_count++;
-		}
-		all->cmd->redirects = all->cmd->redirects->next;
-	}
-	return (last);
+    i = 0;
+    while (heredocs[i])
+    {
+        int is_last = (heredocs[i + 1] == NULL);
+        int fd = is_last ? pipefd[1] : -1;
+        read_heredoc_input(fd, heredocs[i], all, is_last);
+        i++;
+    }
 }
 
-void	heredoc_loop(t_all *all, char **last_heredocs, int heredoc_pipe[2], t_redirect *redir)
-{
-	int i;
-	int n;
 
-	i = ft_strlen(last_heredocs);
-	n = 0;
-	while (n < i && last_heredocs[n])
-	{
-		read_heredoc_input(heredoc_pipe[1], last_heredocs[n], all);
-		n++;
-	}
+/* Tüm heredoc redirect’lerini sayar ve bir char ** dizi döndürür.
+   Dizi sonuna NULL eklenir; her eleman orijinal filename’lere işaret eder. */
+static char **collect_heredocs(t_all *all)
+{
+    t_redirect  *tmp;
+    char        **heredocs;
+    int          count;
+    int          i;
+
+    /* redirect listesini yürüyerek kaç adet heredoc olduğunu bulalım */
+    tmp = all->cmd->redirects;
+    count = 0;
+    while (tmp)
+    {
+        if (tmp->type == HEREDOC)
+            count++;
+        tmp = tmp->next;
+    }
+    heredocs = malloc(sizeof(char *) * (count + 1));
+    if (!heredocs)
+        return (NULL);
+    /* heredoc isimlerini diziye kopyalayalım */
+    tmp = all->cmd->redirects;
+    i = 0;
+    while (tmp)
+    {
+        if (tmp->type == HEREDOC)
+            heredocs[i++] = tmp->filename;
+        tmp = tmp->next;
+    }
+    heredocs[i] = NULL;
+    return (heredocs);
 }
-int	handle_heredoc_process(t_redirect *redir, t_all *all)
-{
-	int		heredoc_pipe[2];
 
-	char **last_heredocs = last_heredoc(all);
-	if (pipe(heredoc_pipe) == -1)
-	{
-		perror("pipe for heredoc");
-		return (1);
-	}
-	heredoc_loop(all, last_heredocs, heredoc_pipe, redir);
-	close(heredoc_pipe[1]);
-	redir->fd = heredoc_pipe[0];
-	if (dup2(redir->fd, STDIN_FILENO) == -1)
-	{
-		perror("dup2 for heredoc");
-		close(redir->fd);
-		return (1);
-	}
-	close(redir->fd);
-	return (0);
+/* heredoc redirect’lerini işler, yalnızca sonuncusunu stdin’e bağlar. */
+int handle_heredoc_process(t_redirect *redir, t_all *all)
+{
+    int   heredoc_pipe[2];
+    char **heredocs;
+
+    heredocs = collect_heredocs(all);
+    if (!heredocs)
+        return (1);
+    if (pipe(heredoc_pipe) == -1)
+    {
+        perror("pipe for heredoc");
+        free(heredocs);
+        return (1);
+    }
+    heredoc_loop(all, heredocs, heredoc_pipe);
+    free(heredocs);
+    close(heredoc_pipe[1]);           /* Yazma ucunu kapat */
+    redir->fd = heredoc_pipe[0];      /* Okuma ucunu redirect’te sakla */
+    if (dup2(redir->fd, STDIN_FILENO) == -1)
+    {
+        perror("dup2 for heredoc");
+        close(redir->fd);
+        return (1);
+    }
+    close(redir->fd);
+    return (0);
 }
 
-int	check_here_flag(t_card *card, char *eof)
+/* here_flag kontrolleri aynı kalır */
+int check_here_flag(t_card *card, char *eof)
 {
-	t_card	*current;
+    t_card  *current;
 
-	current = card;
-	while (current)
-	{
-		if (current->here_flag == 1 && strcmp(current->value, eof) == 0)
-			return (0);
-		current = current->next;
-	}
-	return (1);
+    current = card;
+    while (current)
+    {
+        if (current->here_flag == 1 && strcmp(current->value, eof) == 0)
+            return (0);
+        current = current->next;
+    }
+    return (1);
 }
